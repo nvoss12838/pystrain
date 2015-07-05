bb58c4dd4416f370f77117589272fcc94edc022f
Nick Voss <nvoss@mail.usf.edu>
2015-07-05 11:54:42 -0400

    begin making the files for the webservice utilities

---
+++ strainutil/Client/client.py
@@ -0,0 +1,15 @@
+import obspy.iris import Client
+'''
+build on obspy to get strainmeter data from iris webservices 
+'''
+
+class Client(object):
+	def __init__():
+		self.client = Client()
+		self.network = 'PB'
+		self.station = station()
+	def fetch(self,start,end):
+		
+
+
+st1 = client.timeseries("PB","B004","T0","RS1",dt,dt2) 
